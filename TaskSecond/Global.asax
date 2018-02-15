2bafa5772cb44ffe655f0dd790377713f8d94f4b
levxset <levxset@gmail.com>
2018-02-15 14:19:55 +0200

    Add project files.

---
+++ TaskSecond/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TaskSecond.MvcApplication" Language="C#" %>
---
+++ TaskSecond/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TaskSecond.Startup))]
+namespace TaskSecond
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
